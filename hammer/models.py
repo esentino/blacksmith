f7ff67cf46f920bda2e223f99f20367567bb44a4
Janusz Janowski <januszjanowski@outlook.com>
2021-03-28 23:24:20 +0200

    add blacksmith model

--- hammer/models.py
+++ hammer/models.py
@@ -1,3 +1,15 @@
+import uuid
+
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Blacksmith(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    level = models.IntegerField(default=1)
+    experience = models.IntegerField(default=0)
+    heating_attribute = models.IntegerField(default=1)
+    holding_attribute = models.IntegerField(default=1)
+    hitting_attribute = models.IntegerField(default=1)
+    shaping_attribute = models.IntegerField(default=1)
+    owner = models.OneToOneField(User, on_delete=models.CASCADE)
